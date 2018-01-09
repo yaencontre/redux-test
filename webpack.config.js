46cda58e7beeaeec3420c08cc8517973cbd6d198
J. Ginés Hernández Galindo <jgines@gmail.com>
2018-01-09 21:51:29 +0100

    add sass compiler

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,28 @@
 const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+//const webpack = require('webpack');
 
 module.exports = {
     entry: {
-        main: path.resolve(__dirname, 'src/js/index.js')
+        main: [
+            path.resolve(__dirname, 'src/js/index.js'),
+            path.resolve(__dirname, 'src/css/style.scss')
+        ]
     },
     output: {
-        path: path.resolve(__dirname, 'dist'),
+        path: path.resolve(__dirname, 'public/dist'),
         filename: 'js/[name].js'
     },
     devtool: 'eval-source-map',
     module: {
         rules: [
+            {
+                test: /\.scss$/,
+                use: ExtractTextPlugin.extract({
+                    fallback: "style-loader",
+                    use: ["css-loader", "sass-loader"]
+                })
+            },
             {
                 test: /\.js$/,
                 use: {
@@ -22,6 +34,11 @@ module.exports = {
             }
         ]
     },
-    plugins: []
+    plugins: [
+        // new webpack.optimize.CommonsChunkPlugin({
+        //     name: 'common'
+        // }),
+        new ExtractTextPlugin("css/[name].css")
+    ]
 };
 
